1c62137be8be76da77ee26f82655d7dc1fc9ff79
Henrique <henrique2204barbosa@gmail.com>
2021-02-06 23:22:24 -0300

    Para finalizar -> Adicionar os comandos para impressão em executedOrders

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,16 @@ int main(int argc, char const *argv[]) {
   // Criação Objetos de Arquivos fornecidos pela Linha de Comando
   ifstream mapFile (argv[1]);
   ifstream orderFile(argv[2]);
-  Map mapa(mapFile);
-  mapa.print();
-  cout<<mapa.move_to(9,9)<<endl;
-  cout << mapa.destroy_alien(4,1) << endl;
+  
   // Criação da Base
-  // Base base(mapFile);
+  Base base(mapFile);
   // base.print_map();
 
   // Criação das ordens
-  // Orders orders(orderFile, base);
+  Orders orders(orderFile, base);
 
   // Impressão recursos da Base
-  // base.print_resources();
+  base.print_resources();
 
   return 0;
 }
